c8d7da9bbf988e4e07b040461633fb21d709925c
Renjini R J <renjinirj071@gmail.com>
2026-08-13 22:15:29 +0530

    Initial portfolio home page

---
+++ main/static/js/script.js
@@ -0,0 +1,15 @@
+new Typed(".typing", {
+    strings: [
+        "AI & Data Science Enthusiast",
+        "Python Developer",
+        "Machine Learning Explorer",
+        "Django Developer"
+    ],
+    typeSpeed: 70, 
+    backSpeed: 40,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|"
+});
