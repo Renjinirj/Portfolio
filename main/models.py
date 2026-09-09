f4fa5e516caac348b90a0ad71678dc51d3b5ef9b
Renjini R J <renjinirj071@gmail.com>
2026-09-09 23:09:42 +0530

    Update portfolio sections and dynamic projects

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.CharField(max_length=200)
+    github_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+    image = models.ImageField(upload_to="projects/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+
+    return render(request, "home.html", {
+        "projects": projects
+    })
